400bda82d5728e3d1fbf16d69c7c043c3c34ceb4
junaiddotnet <junaidplus@gmail.com>
2019-10-14 22:29:49 +0100

    first

---
+++ src/components/HelloWorldone.vue
@@ -0,0 +1,21 @@
+<template>
+<h1>
+Test Template
+</h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorldone',
+  hour: new Date().getHours(),
+
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      isAfterNoon:true,
+      IsMorning:true,
+      Isnight:false
+    }
+  }
+}
+</script>
